0beca8ab4b9d6a4884c29735fec699142036c5d4
MrZhouZh <17600330793@sina.cn>
2023-08-21 23:31:46 +0800

    feat: add skeleton

---
+++ components/SkeletonView/index.vue
@@ -0,0 +1,189 @@
+<script setup lang="ts">
+type ViewStatus =
+  | 'loading'
+  | 'error'
+  | 'success'
+  | 'empty'
+
+interface SkeletonProps<T = any> {
+  status: ViewStatus
+  result: T
+  placeholderResult: T
+  emptyMsg?: string
+  errorMsg?: string
+  isEmpty?: (result: T) => boolean
+}
+
+const props = withDefaults(defineProps<SkeletonProps>(), {
+  status: 'loading',
+  emptyMsg: 'empty data',
+  errorMsg: 'unknown error',
+})
+
+const emits = defineEmits(['retry'])
+
+function retryClick() {
+  emits('retry')
+}
+
+const viewStatus = computed<ViewStatus | undefined>(() => {
+  const status = props.status
+  if (status === 'success') {
+    let _isEmpty = false
+    const result = props.result
+    if (props.isEmpty) {
+      _isEmpty = props.isEmpty(props.result)
+    }
+    else {
+      if (Array.isArray(result))
+        _isEmpty = result.length === 0
+
+      else if (!result)
+        _isEmpty = true
+
+      else
+        _isEmpty = false
+    }
+
+    if (_isEmpty)
+      return 'empty'
+
+    return 'success'
+  }
+  return status
+})
+
+const placeholderData = computed(() => {
+  if (props.result)
+    return props.result
+
+  return props.placeholderResult
+})
+</script>
+
+<template>
+  <div v-if="viewStatus === 'empty'" key="empty" class="empty-view flex-col">
+    <span>{{ emptyMsg }}</span>
+    <button btn mt4 max-w-160px @click="retryClick">
+      Retry
+    </button>
+  </div>
+  <div v-else-if="viewStatus === 'error'" key="error" class="empty-view flex-col">
+    <span>{{ errorMsg }}</span>
+    <button btn mt4 max-w-160px @click="retryClick">
+      Retry
+    </button>
+  </div>
+  <div
+    v-else
+    key="loadingOrContent"
+    :class="[
+      placeholderData && viewStatus === 'loading'
+        ? 'skeleton-view-empty-view'
+        : 'skeleton-view-default-view',
+    ]"
+  >
+    <div
+      v-if="!placeholderData && viewStatus === 'loading'"
+      class="loading-center"
+    >
+      <i carbon:fade text-40px text-blue />
+    </div>
+    <slot
+      v-else
+      :result="placeholderData"
+      :status="viewStatus"
+      :success="viewStatus === 'success'"
+      :mask="viewStatus === 'loading' ? 'skeleton-mask' : ''"
+    />
+  </div>
+</template>
+
+<style>
+.empty-view {
+  padding-top: 50px;
+  padding-bottom: 50px;
+  align-items: center;
+}
+.empty-img {
+  width: 310px;
+  height: 218px;
+}
+.trip-text {
+  font-size: 20px;
+  color: #999;
+}
+.loading-center {
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.skeleton-view-default-view span,
+.skeleton-view-default-view a,
+.skeleton-view-default-view img,
+.skeleton-view-default-view td,
+.skeleton-view-default-view button {
+  transition-duration: 0.7s;
+  transition-timing-function: ease;
+  transition-property: background, width;
+}
+.skeleton-view-empty-view {
+  position: relative;
+  pointer-events: none;
+}
+
+.skeleton-view-empty-view::before {
+  content: " ";
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background: linear-gradient(
+    110deg,
+    rgba(255, 255, 255, 0.1) 40%,
+    rgba(180, 199, 255, 0.3) 50%,
+    rgba(255, 255, 255, 0.1) 60%
+  );
+  background-size: 200% 100%;
+  background-position-x: 180%;
+  animation: loading 1s ease-in-out infinite;
+  z-index: 1;
+}
+@keyframes loading {
+  to {
+    background-position-x: -20%;
+  }
+}
+.skeleton-view-empty-view .skeleton-mask {
+  position: relative;
+}
+.skeleton-view-empty-view .skeleton-mask::before {
+  content: " ";
+  background-color: #f5f5f5;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  border: 1px solid #f5f5f5;
+  top: -1px;
+  left: -1px;
+  z-index: 1;
+}
+
+.skeleton-view-empty-view button,
+.skeleton-view-empty-view span,
+.skeleton-view-empty-view input,
+.skeleton-view-empty-view td,
+.skeleton-view-empty-view a {
+  color: rgba(0, 0, 0, 0) !important;
+  border: none;
+  background: #f5f5f5 !important;
+}
+/* [src=""],img:not([src])*/
+.skeleton-view-empty-view img {
+  content: url(./no_url.png);
+  border-radius: 2px;
+  background: #f5f5f5 !important;
+}
+</style>
---
+++ composables/useAutoSkeletonView.ts
@@ -0,0 +1,134 @@
+import type { UnwrapRef } from 'vue-demi'
+
+type ApiFn<D, P extends any[]> = (...params: P) => Promise<D>
+
+export type SkeletonStatus = 'loading' | 'success'
+
+export interface UseAutoSkeletonViewProps<D, P extends any[]> {
+  apiFn: ApiFn<D, P>
+  placeholderResult?: D
+  queryInMount?: boolean
+  initQueryParams?: P
+  transformDataFn?: (data: D) => Promise<D>
+  updateParamsOnFetch?: boolean
+  defaultStatus?: SkeletonStatus
+  onSuccess?: (data: any) => void
+  isEmpty?: (data: D) => boolean
+}
+
+export type AutoSkeletonViewResult<D, P extends any[]> = UnwrapRef<{
+  execute: ApiFn<D, P>
+  result: D | null
+  retry: () => Promise<D>
+  loading: boolean
+  status: SkeletonStatus | 'error'
+  getField: (key: string) => any
+  bindProps: {
+    result: D | null
+    status: SkeletonStatus | 'error'
+    errorMsg: string
+    placeholderResult?: D
+    isEmpty?: (data: D) => boolean
+  }
+  bindEvents: {
+    retry: () => Promise<D>
+  }
+}>
+
+export function UseAutoSkeletonView<D, P extends any[] = any[]>(
+  props: UseAutoSkeletonViewProps<D, P>,
+): AutoSkeletonViewResult<D, P> {
+  const {
+    apiFn,
+    defaultStatus = 'loading',
+    placeholderResult,
+    isEmpty,
+    initQueryParams = [],
+    transformDataFn,
+    onSuccess,
+    updateParamsOnFetch = true,
+    queryInMount = true,
+  } = props
+
+  const status = ref<SkeletonStatus | 'error'>(defaultStatus)
+
+  const result = ref<D | null>(null)
+
+  const placeholder = ref<D | undefined>(placeholderResult)
+
+  const errorMsg = ref('')
+
+  const lastFetchParams = ref<any[]>(initQueryParams)
+  // TODO: type UnwrapRef<P>
+  // const lastFetchParams = ref<P>(initQueryParams as P)
+
+  const executeApiFn: ApiFn<D, P> = (...params: P) => {
+    if (updateParamsOnFetch)
+      lastFetchParams.value = params
+
+    status.value = 'loading'
+
+    return apiFn(...params)
+      .then((res) => {
+        let data: any = res
+        if (transformDataFn)
+          data = transformDataFn(res)
+
+        placeholder.value = data
+        result.value = data
+        status.value = 'success'
+        onSuccess && onSuccess(data)
+        return res
+      })
+      .catch((e) => {
+        console.error('--useAutoSkeletonView--', e)
+        status.value = 'error'
+        errorMsg.value = e.message
+        throw e
+      })
+  }
+
+  function retry() {
+    return executeApiFn(...(lastFetchParams.value as P))
+  }
+
+  onMounted(() => {
+    if (queryInMount && defaultStatus === 'loading')
+      executeApiFn(...(initQueryParams as P))
+  })
+
+  const loading = computed(() => {
+    return status.value === 'loading'
+  })
+
+  function getField(key: string) {
+    if (status.value !== 'success')
+      return ''
+
+    if (result.value)
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment, @typescript-eslint/prefer-ts-expect-error
+      // @ts-ignore
+      return result.value[key]
+
+    return ''
+  }
+
+  return reactive({
+    execute: executeApiFn,
+    result,
+    retry,
+    loading,
+    status,
+    getField,
+    bindProps: {
+      result,
+      status,
+      errorMsg,
+      placeholderResult: placeholder,
+      isEmpty,
+    },
+    bindEvents: {
+      retry,
+    },
+  })
+}
